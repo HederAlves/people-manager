7f757fc15917b0b23419abb92241ef2ec8a6e100
Heder Alves <alvesheder92@gmail.com>
2024-04-09 23:23:53 -0300

    feat: add label to input login

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -1,9 +1,11 @@
 <template>
-  <v-container class="flex flex-col gap-5 justify-center min-h-full">
+  <v-container class="flex flex-col gap-52 justify-center min-h-full">
     <h1 class="text-h5 text-center font-weight-bold mt-10">Login</h1>
     <v-col class="text-center">
       <v-form @submit.prevent="login">
+        <label>Email</label>
         <input class="text-black bg-white mx-4 " v-model="email" label="Email" type="email" />
+        <label>Senha</label>
         <input class="text-black bg-white mx-4" v-model="password" label="Password" type="password" />
         <v-btn type="submit" color="primary">Entrar</v-btn>
         <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
